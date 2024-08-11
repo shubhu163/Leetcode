d1a33842b2818c50405ed72168f4acb6fcf03985
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2024-08-10 21:16:17 -0700

    Time: 32 ms (65.11%) | Memory: 13.5 MB (19.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,5 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return True if sorted(list(s)) == sorted(list(t)) else False
+        # My solution - return True if sorted(list(s)) == sorted(list(t)) else False 
+
+        # Neetcode Solution  - 
+        if len(s) != len(t):
+            return False
+        c_T,c_S = {},{}
+        for i in range(len(s)):
+            c_S[s[i]] = 1 + c_S.get(s[i],0)
+            c_T[t[i]] = 1 + c_T.get(t[i],0)
+        for c in c_S:
+            if c_S[c] != c_T.get(c,0):
+                return False
+
+        return True
+
+        
 
