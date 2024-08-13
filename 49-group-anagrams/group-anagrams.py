d2a184894d332caad87158a407490436c3b37265
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2024-08-12 22:39:47 -0700

    Time: 85 ms (20.76%) | Memory: 17.9 MB (12.33%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list)
+
+        for s in strs:
+            
+            count = [0] * 26
+
+            for i in s:
+                count[ord(i) - ord('a')] +=1
+            
+            res[tuple(count)].append(s)
+        return res.values()
+
+        
