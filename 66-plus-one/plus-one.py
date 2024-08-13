cd72492678dfe3c7b23a94be539671f2c87d0c94
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2024-08-12 23:44:59 -0700

    Time: 7 ms (98.48%) | Memory: 11.7 MB (6.65%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,10 +4,20 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        a = ''
-        for i in digits:
-            a += str(i)
+        # a = ''
+        # for i in digits:
+        #     a += str(i)
         
-        a = int(a) + 1
-        return list(map(int, list(str(a))))
+        # a = int(a) + 1
+        # return list(map(int, list(str(a))))
 
+        for i in range(len(digits) - 1, -1, -1):
+                # If the digit is less than 9, just increment and return
+                if digits[i] < 9:
+                    digits[i] += 1
+                    return digits
+                # If the digit is 9, set it to 0 and continue the loop
+                digits[i] = 0
+            
+            # If all digits were 9, we need to add a new digit at the beginning
+        return [1] + digits
