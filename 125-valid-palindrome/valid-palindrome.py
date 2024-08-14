96b97a29f02cb1587ebc49d3c6a97157a5ff8018
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2024-08-13 20:32:20 -0700

    Time: 288 ms (24.91%) | Memory: 12.9 MB (62.46%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,12 +5,17 @@ class Solution(object):
         :rtype: bool
         """
         # My Solution
-        n = s.strip().replace(' ','').lower()
-        for d in n:
-            if ord(d) not in range(97,123) and ord(d) not in range(48,58):
-                n = n.replace(d,'')
-        b = n
-        if n == b[::-1]:
-            return True
+        # n = s.strip().replace(' ','').lower()
+        # for d in n:
+        #     if ord(d) not in range(97,123) and ord(d) not in range(48,58):
+        #         n = n.replace(d,'')
+        # b = n
+        # if n == b[::-1]:
+        #     return True
 
-        return False
+        # return False
+        n = ''
+        for c in s:
+            if c.isalnum():
+                n+=c.lower()
+        return n == n[::-1]
