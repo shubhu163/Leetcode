244a0d8a5053d4eab8e1e8661fc652eb5431625d
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2025-01-26 23:22:32 -0800

    Time: 4 ms (5.52%) | Memory: 12.4 MB (73.88%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        remove = '1234567890.,\'!&*?/"'
+
+        i = len(s) - 1
+        count = 0
+
+        if len(s) == 1:
+            return 1
+        print(s)
+
+        while i>-1:
+            if s[i] in remove:
+               i -= 1
+
+            
+            if ord(s[i].lower()) >= ord('a') and ord(s[i].lower()) <= ord('z'):
+                count+=1
+                i -= 1
+            
+            else:       
+                break
+        
+        return count
+
+
+
+        
