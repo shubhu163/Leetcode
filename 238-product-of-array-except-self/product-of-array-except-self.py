41d2de68b0da43aca585a90305371b6cb7afcf13
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2025-07-05 19:01:41 -0700

    Time: 47 ms (25.62%) | Memory: 22 MB (31.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,16 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = [1] * len(nums)
-        prefix = 1
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
+        tot = 1
 
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums)-1,-1,-1):
-            res[i] *= postfix
-            postfix *= nums[i] 
-        return res
+            tot = 1 if i == 0 else prefix[i-1] * nums[i-1]
+            prefix[i] = tot
 
-        #This solution has O(1) space complexity because output array is not included in space and time complexity is O(2n) ~= O(n)
+        nums2 = nums[::-1]
+        for i in range(len(nums2)):
+            tot = 1 if i == 0 else suffix[i-1] * nums2[i-1]
+            suffix[i] = tot
+        
+        suffix.reverse()
+        for i in range(len(nums)):            
+            nums[i] = suffix[i] * prefix[i]
+        return nums
+
+
+
+
+
+        
+
+        
+
+
+        
+        
