de8afaf9a12bd5d81a3cabccf24e4f302ce38e9a
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2025-07-05 19:13:08 -0700

    Time: 31 ms (68.31%) | Memory: 20.1 MB (84.41%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,23 +4,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        prefix = [1] * len(nums)
-        suffix = [1] * len(nums)
-        tot = 1
+        # prefix = [1] * len(nums)
+        # suffix = [1] * len(nums)
+        # tot = 1
 
-        for i in range(len(nums)):
-            tot = 1 if i == 0 else prefix[i-1] * nums[i-1]
-            prefix[i] = tot
+        # for i in range(len(nums)):
+        #     tot = 1 if i == 0 else prefix[i-1] * nums[i-1]
+        #     prefix[i] = tot
 
-        nums2 = nums[::-1]
-        for i in range(len(nums2)):
-            tot = 1 if i == 0 else suffix[i-1] * nums2[i-1]
-            suffix[i] = tot
+        # nums2 = nums[::-1]
+        # for i in range(len(nums2)):
+        #     tot = 1 if i == 0 else suffix[i-1] * nums2[i-1]
+        #     suffix[i] = tot
         
-        suffix.reverse()
-        for i in range(len(nums)):            
-            nums[i] = suffix[i] * prefix[i]
-        return nums
+        # suffix.reverse()
+        # for i in range(len(nums)):            
+        #     nums[i] = suffix[i] * prefix[i]
+        # return nums
+
+        nums2 = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            nums2[i] = prefix
+            prefix = prefix * nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            nums2[i] = nums2[i] * postfix
+            postfix = postfix * nums[i]
+
+        return nums2
 
 
 
