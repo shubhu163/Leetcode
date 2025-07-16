34c2533ec5512f2fb553d4bb65462c61d2462723
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2025-07-15 21:15:03 -0700

    Time: 28 ms (76.81%) | Memory: 15.2 MB (31.65%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -5,15 +5,15 @@ class Solution(object):
         :rtype: int
         """
         ROWS, COLS = len(grid), len(grid[0])
-        areas = []
+        max_area = 0
 
         def dfs(r, c):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == 0:
                 return 0
 
-            grid[r][c] = 0  # mark as visited
+            grid[r][c] = 0  # Mark visited
 
-            # recursively count the area
+            # Explore 4 directions and sum the area
             area = 1
             area += dfs(r - 1, c)
             area += dfs(r + 1, c)
@@ -24,7 +24,6 @@ class Solution(object):
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    area = dfs(r, c)
-                    areas.append(area)
+                    max_area = max(max_area, dfs(r, c))
 
-        return max(areas) if areas else 0
+        return max_area
