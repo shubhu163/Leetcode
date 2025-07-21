bdd983ca03e68a754587e78e0fbf46bddd78409b
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2025-07-20 17:26:17 -0700

    Time: 7 ms (55.44%) | Memory: 21.5 MB (88.18%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,30 +1,25 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
-        visited = set()
-
-        def dfs(r, c, region):
-            # If it's outside, then the region is not surrounded
-            if r < 0 or r >= ROWS or c < 0 or c >= COLS:
-                return False
-            if board[r][c] == 'X' or (r, c) in visited:
-                return True
-
-            visited.add((r, c))
-            region.append((r, c))
-
-            up = dfs(r - 1, c, region)
-            down = dfs(r + 1, c, region)
-            left = dfs(r, c - 1, region)
-            right = dfs(r, c + 1, region)
-
-            return up and down and left and right
+        
+        def dfs(r,c):
+            if min(r,c) < 0 or r == ROWS or c == COLS or board[r][c] != 'O':
+                return 
+            board[r][c] = 'T'
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)
 
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == 'O' and (r, c) not in visited:
-                    region = []
-                    surrounded = dfs(r, c, region)
-                    if surrounded:
-                        for x, y in region:
-                            board[x][y] = 'X'
+                if board[r][c] == 'O' and (r in [0,ROWS-1] or c in [0,COLS-1]):
+                    dfs(r,c)
+
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
