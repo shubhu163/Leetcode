fd322ebc65b667463d2d60efdc13024fdaaf7a5e
Shubhankar Joshi <71623089+shubhu163@users.noreply.github.com>
2025-08-05 19:58:39 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (48.13%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -1,33 +1,39 @@
-class Solution(object):
-    def zigzagLevelOrder(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
-        if not root:
-            return []
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         queue = deque()
-        queue.append(root)
+
+        if root:
+            queue.append(root)
+        
         res = []
-        count = 0
-
+        flag = True
         while queue:
-            count += 1
-            size = len(queue)
             arr = []
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                if curr:
+                    arr.append(curr.val)
 
-            for i in range(size):
-                node = queue.popleft()
-                arr.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            if count % 2 == 0:
-                arr.reverse()
-            res.append(arr)
-
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            
+            if flag == True:
+                res.append(arr)
+            else:
+                arr2 = []
+                for i in range(len(arr)-1,-1,-1):
+                    arr2.append(arr[i])
+                res.append(arr2)
+            flag = not flag
+        
         return res
+                
+        
